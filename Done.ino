340582cd2561088b31b93e31972862c5b322307b
tronghhh41-cloud <tronghhh41@gmail.com>
2026-02-09 16:12:45 +0700

    Uploaded

--- Done.ino
+++ Done.ino
@@ -1,123 +1,127 @@
 #include <Wire.h>
-#include <ESP32Servo.h>
-#include <esp_now.h>
 #include <WiFi.h>
+#include <esp_now.h>
+#include <ESP32Servo.h>
 
-#include <MPU6050.h>
-#include <HMC5883L.h>
+#include <MPU6050.h>                 // I2Cdev version
+#include <Adafruit_Sensor.h>
+#include <Adafruit_HMC5883_U.h>      // THAY cho HMC5883L
 #include <Adafruit_BMP280.h>
 #include <TinyGPSPlus.h>
+#include <Adafruit_NeoPixel.h>
 
-/* ================= MOTOR ================= */
+/* ========= PIN ========= */
 #define M1_PIN 4
 #define M2_PIN 5
-#define M3_PIN 6
-#define M4_PIN 7
+#define M3_PIN 16
+#define M4_PIN 17
 
+#define LED_PIN 38
+#define LED_COUNT 1
+
+#define BAT_ADC 1   // chân đo pin (qua chia áp)
+
+/* ========= OBJECT ========= */
 Servo m1, m2, m3, m4;
-
-/* ================= LED RGB (ONBOARD) ================= */
-#define LED_R 10
-#define LED_G 11
-#define LED_B 12
-
-void setLED(bool r, bool g, bool b) {
-  digitalWrite(LED_R, r);
-  digitalWrite(LED_G, g);
-  digitalWrite(LED_B, b);
-}
-
-/* ================= PIN ================= */
-#define PIN_BAT 1   // ADC đo pin (qua chia áp)
-
-/* ================= SENSOR ================= */
 MPU6050 mpu;
-HMC5883L compass;
+Adafruit_HMC5883_Unified compass = Adafruit_HMC5883_Unified(12345);
 Adafruit_BMP280 bmp;
-TinyGPSPlus gps;
-HardwareSerial GPSSerial(1);
 
-/* ================= RX STRUCT ================= */
+TinyGPSPlus gps;
+HardwareSerial GPS_Serial(1);
+
+Adafruit_NeoPixel led(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+/* ========= RX DATA ========= */
 typedef struct {
-  int ch1; // roll
-  int ch2; // pitch
-  int ch3; // throttle
-  int ch4; // yaw
+  int roll;
+  int pitch;
+  int throttle;
+  int yaw;
   bool arm;
 } RXData;
 
 RXData rx;
 unsigned long lastRX = 0;
 
-/* ================= STATE ================= */
+/* ========= STATE ========= */
 bool armed = false;
 bool homeSet = false;
-bool gpsFix = false;
 
-/* ================= HOME ================= */
+/* ========= HOME ========= */
 float homeLat, homeLng, homeAlt;
 
-/* ================= IMU ================= */
+/* ========= IMU ========= */
 float angleX = 0, angleY = 0;
 unsigned long lastIMU = 0;
 
-/* ================= ESP-NOW ================= */
-void onDataRecv(const esp_now_recv_info_t *info, const uint8_t *data, int len) {
+/* ========= ESP-NOW ========= */
+void onDataRecv(const esp_now_recv_info_t *info,
+                const uint8_t *data, int len) {
   if (len == sizeof(RXData)) {
     memcpy(&rx, data, sizeof(rx));
     lastRX = millis();
   }
 }
 
-/* ================= GPS + COMPASS ================= */
-float getBearing(float lat1, float lon1, float lat2, float lon2) {
-  float dLon = radians(lon2 - lon1);
-  lat1 = radians(lat1);
-  lat2 = radians(lat2);
-  float y = sin(dLon) * cos(lat2);
-  float x = cos(lat1) * sin(lat2) -
-            sin(lat1) * cos(lat2) * cos(dLon);
-  float brng = degrees(atan2(y, x));
-  return fmod(brng + 360, 360);
-}
-
+/* ========= UTILS ========= */
 float getHeading() {
-  Vector mag = compass.readNormalize();
-  float h = atan2(mag.YAxis, mag.XAxis) * 57.3;
+  sensors_event_t event;
+  compass.getEvent(&event);
+
+  float h = atan2(event.magnetic.y, event.magnetic.x) * 57.3;
   if (h < 0) h += 360;
   return h;
 }
 
-/* ================= BATTERY ================= */
-float readBattery() {
-  int adc = analogRead(PIN_BAT);
-  return adc * 3.3 / 4095.0 * 2.0; // chia áp 1/2
+float getBearing(float lat1, float lon1,
+                 float lat2, float lon2) {
+  float dLon = radians(lon2 - lon1);
+  lat1 = radians(lat1);
+  lat2 = radians(lat2);
+
+  float y = sin(dLon) * cos(lat2);
+  float x = cos(lat1) * sin(lat2) -
+            sin(lat1) * cos(lat2) * cos(dLon);
+
+  float b = degrees(atan2(y, x));
+  return fmod(b + 360, 360);
 }
 
-/* ================= SETUP ================= */
-void setup() {
-  pinMode(LED_R, OUTPUT);
-  pinMode(LED_G, OUTPUT);
-  pinMode(LED_B, OUTPUT);
+float readBattery() {
+  int raw = analogRead(BAT_ADC);
+  float v = raw * 3.3 / 4095.0;
+  return v * 2.0; // chia áp 1:1
+}
 
+/* ========= SETUP ========= */
+void setup() {
   Wire.begin(8, 9);
   Wire.setClock(400000);
 
   mpu.initialize();
-  compass.initialize();
+
+  if (!compass.begin()) {
+    while (1) delay(10);
+  }
+
   bmp.begin(0x76);
 
-  GPSSerial.begin(9600, SERIAL_8N1, 18, 17);
+  GPS_Serial.begin(9600, SERIAL_8N1, 18, 17);
 
-  m1.attach(M1_PIN);
-  m2.attach(M2_PIN);
-  m3.attach(M3_PIN);
-  m4.attach(M4_PIN);
+  m1.attach(M1_PIN, 1000, 2000);
+  m2.attach(M2_PIN, 1000, 2000);
+  m3.attach(M3_PIN, 1000, 2000);
+  m4.attach(M4_PIN, 1000, 2000);
 
   m1.writeMicroseconds(1000);
   m2.writeMicroseconds(1000);
   m3.writeMicroseconds(1000);
   m4.writeMicroseconds(1000);
+
+  led.begin();
+  led.clear();
+  led.show();
 
   WiFi.mode(WIFI_STA);
   esp_now_init();
@@ -127,25 +131,20 @@ void setup() {
   lastIMU = millis();
 }
 
-/* ================= LOOP ================= */
+/* ========= LOOP ========= */
 void loop() {
+  while (GPS_Serial.available())
+    gps.encode(GPS_Serial.read());
 
-  /* ===== GPS ===== */
-  while (GPSSerial.available())
-    gps.encode(GPSSerial.read());
+  bool lost = millis() - lastRX > 700;
 
-  gpsFix = gps.location.isValid() && gps.satellites.value() >= 6;
-
-  if (!homeSet && gpsFix) {
+  /* ===== SET HOME ===== */
+  if (!homeSet && gps.location.isValid()) {
     homeLat = gps.location.lat();
     homeLng = gps.location.lng();
     homeAlt = bmp.readAltitude(1013.25);
     homeSet = true;
   }
-
-  bool lost = millis() - lastRX > 700;
-  float batt = readBattery();
-  bool lowBat = batt < 3.4;
 
   /* ===== IMU ===== */
   unsigned long now = millis();
@@ -161,43 +160,44 @@ void loop() {
   angleX = 0.98 * (angleX + gx / 131.0 * dt) + 0.02 * accX;
   angleY = 0.98 * (angleY + gy / 131.0 * dt) + 0.02 * accY;
 
-  int throttle = rx.ch3;
+  int throttle = rx.throttle;
   int rollAdj  = angleX * 2.0;
   int pitchAdj = angleY * 2.0;
-  int yawAdj   = rx.ch4;
+  int yawAdj   = rx.yaw;
 
-  /* ===== FAILSAFE + RTH ===== */
-  if ((lost || lowBat) && armed && homeSet && gpsFix) {
-    setLED(1,0,0); // 🔴 FAILSAFE
-
+  /* ===== FAILSAFE RTH ===== */
+  if (lost && armed && homeSet && gps.location.isValid()) {
     float bearing = getBearing(
-      gps.location.lat(),
-      gps.location.lng(),
-      homeLat, homeLng
-    );
+      gps.location.lat(), gps.location.lng(),
+      homeLat, homeLng);
 
     float yawErr = bearing - getHeading();
     if (yawErr > 180) yawErr -= 360;
     if (yawErr < -180) yawErr += 360;
 
-    yawAdj = constrain(yawErr * 2.0, -40, 40);
-    pitchAdj -= 12;
-    throttle = 1400;
+    yawAdj = constrain(yawErr * 2, -40, 40);
+    pitchAdj -= 15;
+
+    throttle = 1420;
+    if (bmp.readAltitude(1013.25) < homeAlt)
+      throttle += 30;
+
+    led.setPixelColor(0, led.Color(255, 0, 0)); // đỏ
   }
-  else if (lowBat) setLED(1,1,0);        // 🟡
-  else if (!rx.arm) setLED(0,0,1);       // 🔵
-  else if (homeSet) setLED(1,0,1);       // 🟣
-  else setLED(0,1,0);                    // 🟢
+
+  /* ===== BATTERY ===== */
+  if (readBattery() < 3.4) {
+    led.setPixelColor(0, led.Color(255, 0, 255)); // tím
+  }
 
   throttle = constrain(throttle, 1100, 1750);
 
-  /* ===== MOTOR OUTPUT ===== */
   if (rx.arm || armed) {
     armed = true;
-    m1.writeMicroseconds(constrain(throttle + rollAdj - pitchAdj + yawAdj,1000,2000));
-    m2.writeMicroseconds(constrain(throttle - rollAdj - pitchAdj - yawAdj,1000,2000));
-    m3.writeMicroseconds(constrain(throttle - rollAdj + pitchAdj + yawAdj,1000,2000));
-    m4.writeMicroseconds(constrain(throttle + rollAdj + pitchAdj - yawAdj,1000,2000));
+    m1.writeMicroseconds(throttle + rollAdj - pitchAdj + yawAdj);
+    m2.writeMicroseconds(throttle - rollAdj - pitchAdj - yawAdj);
+    m3.writeMicroseconds(throttle - rollAdj + pitchAdj + yawAdj);
+    m4.writeMicroseconds(throttle + rollAdj + pitchAdj - yawAdj);
   } else {
     armed = false;
     m1.writeMicroseconds(1000);
@@ -206,5 +206,6 @@ void loop() {
     m4.writeMicroseconds(1000);
   }
 
+  led.show();
   delay(5);
 }
